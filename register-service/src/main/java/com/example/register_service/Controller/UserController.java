6606547f630bbc82b5987e19da366861eb29d7a6
TinyAntu <alonso.henriquez.h@usach.cl>
2024-11-28 13:22:36 -0300

    cambios

--- register-service/src/main/java/com/example/register_service/Controller/UserController.java
+++ register-service/src/main/java/com/example/register_service/Controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/register")
-
+//@CrossOrigin(origins="http://localhost:5173")
 public class UserController {
     @Autowired
     UserService userService;
